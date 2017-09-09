234c7166d0c6326c554e159c982f4fe2027ad712
zt19994 <zt191610942@163.com>
2017-09-09 18:07:23 +0800

    two

--- qidian.py
+++ qidian.py
@@ -4,19 +4,44 @@
 import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import RequestException
+import pprint
 
-url = 'http://a.qidian.com/'
 
-def get_page_index(url):
+def get_page(url):
     try:
         res = requests.get(url)
         res.encoding = 'utf-8'
         if res.status_code == 200:
             return res.text
-        return None
     except RequestException:
         print('请求失败')
         return None
 
+
+def parser_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    books = []
+    for i in range(20):
+        book = {}
+        book['title'] = soup.select(' h4 a')[i].text
+        book['author'] = soup.select('a.name ')[2:][i].text
+        book['type'] = soup.select('a.go-sub-type ')[i].text
+        book['words'] = soup.select('.update span')[i].text.rstrip('万字')
+        books.append(book)
+    return books
+
+def get_next_page(page_number):
+    url = 'http://a.qidian.com?orderId=&style=1&pageSize=20&siteid=1&hiddenField=0&page={}'.format(page_number)
+    return url
+
+
+
 def main():
-    print(get_page_index(url))
+    for i in range(20):
+        url = get_next_page(i)
+        html = get_page(url)
+        pprint.pprint(parser_page(html))
+
+
+if __name__ == '__main__':
+    main()
